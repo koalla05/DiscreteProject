229001bdffbdcb93381b4893a21381a8f6c103d9
koalla05 <kov.koalla@gmail.com>
2024-04-19 22:15:35 +0300

    Generaating graph in matrix and lists

---
+++ MaxFlow/Program.cs
@@ -0,0 +1,5 @@
+﻿using MaximumFlow;
+var graph = new Generation(5, 50);
+var adjacencyMatrix = graph.AdjacencyMatrix();
+var adjacencyList = graph.AdjacencyList(adjacencyMatrix);
+Console.WriteLine();
